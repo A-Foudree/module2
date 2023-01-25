dd74de6f330a37335c9d133086e5b52648834ee9
Ashley Fry <adfry@dmacc.edu>
2023-01-24 23:28:08 -0600

    assignment done in IDE PY- average_scores.py need to pay attention to indents needed, spelling errors and double check proper spacing.

--- Format_output/average_scores.py
+++ Format_output/average_scores.py
@@ -1,5 +1,12 @@
-
 """
+Program: average_scores.py
+Author: Ashley Fry
+Last date modified: 01/24/2023
+The purpose of this program is to write the manual tests and program average_scores.py to read
+in one person's names, first and last, their age and three scores out of 100. Take the three scores
+and find the average, storing into a variable.
+
+
 It's time to write the main in your average_scores.py.
 Prompt the user for what is expected for each input
 Store into local variables last_name, first_name, age.
@@ -8,54 +15,25 @@ Use a constant to represent the number of scores you will prompt the user to inp
 Prompt the user for the scores, storing them in local variable or variables.
 You can keep separate variables for each score, or you keep a running total in one variable.
 Calcuate the average using the variables.
-Print the output, with last name followed by a comma and the first name followed by an integer age and then the average scores with 2 decimal places.
+Print the output, with last name followed by a comma and the first name followed by an integer age
+and then the average scores with 2 decimal places.
 Example output:
 Rodriguez, Linda age: 21 average grade: 92.50
-Add doctring to the top of your file, add comments at the bottom with observed output after a few manual test runs of your code.
+Add doctring to the top of your file, add comments at the bottom with observed output after a few manual
+ test runs of your code.
 Submit your average_scores.py
 """
 
-"""
-Below I took notes, made comments, wrote pseudocode.
-"""
-#Store a constant of the number of scores we will input: 3
+def main():
+    Last_name = input("Enter Last Name : ")
+    First_name = input("Enter Fist Name : ")
 
-#Prompt user for inputs
-
-    #Store input as last_name
-    #Store input as first_name
-
-    #Prompt users for the scores ( do it constant times, using a new variable each time)
-
-#Calculate an average (sum each value and divid by the number of values)
-
-#Print output; should look like
-#Rodriguez, Linda age: 21 average grade: 92.50
-
-
-"""
-Examples of what we are doing.
-"""
-
-#Store a constant of the number of scores we will input: 3
-PI = 3.14
-
-#Prompt user for inputs
-
-    #Store input as last_name
-age = input("Enter your age: ")
-    #Store input as first_name
-age = input("Enter your age: ")
-    #add an age input
-age = input("Enter your age: ")
-
-    #Prompt users for the scores ( do it constant times, using a new variable each time)
-#score one -  age_as_int= int(input("confirms age please:"))
-#score two -  age_as_int= int(input("confirms age please:"))
-#score three- age_as_int= int(input("confirms age please:"))
-
-#Calculate an average (sum each value and divid by the number of values)
-
-#Print output; should look like
-#Rodriguez, Linda age: 21 average grade: 92.50
-#print(f'Geeks: {geeks_points}, Portal: {portals}')
+    age = int(input("Enter your age : "))
+    num_scores = 3
+    score1 = int(input("Enter first score: "))
+    score2 = int(input("Enter second score: "))
+    score3 = int(input("Enter third score : "))
+    ave = (score1 + score2 + score3)/num_scores
+    print(Last_name,",",First_name,"age:",age,"average grade:%.2f"%ave)
+    if __name__ == "__main__":
+        main()
