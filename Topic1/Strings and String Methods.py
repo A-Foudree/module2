c47a7d59574ec7a39b94f09deac402ba814230b5
Ashley Fry <adfry@dmacc.edu>
2023-01-24 20:40:47 -0600

    assignment done in IDE PY- finished list and list methods

---
+++ Topic1/Strings and String Methods.py
@@ -0,0 +1,26 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x = 'fanny Chenal in a Good Year'
+print(x.capitalize())
+Fanny chenal in a good year
+x.find("an")
+1
+x.index("in")
+13
+x.isalnum()
+False
+
+x.isalpha()
+False
+x.isdigit()
+False
+
+x.islower()
+False
+x.isupper()
+False
+
+x.isspace()
+False
+x.startswith('fanny')
+True
